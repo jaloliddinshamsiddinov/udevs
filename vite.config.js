6f365192d969ff5c956a4b06d9e99109d0828492
Jaloliddin <hi.shamsiddinov@gmail.com>
2024-07-14 22:45:26 +0500

    fix/vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   optimizeDeps: {
     exclude: ['js-big-decimal']
   }
