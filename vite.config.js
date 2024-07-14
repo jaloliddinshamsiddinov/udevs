985454f42dd5752a2625e51041fffe2d0480e5b2
Jaloliddin <hi.shamsiddinov@gmail.com>
2024-07-14 23:48:43 +0500

    fix/tools img path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
   },
   optimizeDeps: {
-    exclude: ['js-big-decimal']
-  }
-})
+    exclude: ["js-big-decimal"],
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
